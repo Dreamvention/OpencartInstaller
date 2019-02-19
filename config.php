436332fe961e680f11795d43bd48be38612bf9d6
Dmitriy Zhuk <dmitriyzhuk@gmail.com>
2019-02-20 01:17:20 +0200

    Update config.php
    updated config

--- config.php
+++ config.php
@@ -1,13 +1,13 @@
 ﻿<?php
 define('USERNAME', 'admin');
-define('PASSWORD', 'demo1234');
-define('EMAIL', 'dmitriyzhuk@gmail.com');
+define('PASSWORD', 'pass');
+define('EMAIL', 'email@gmail.com');
 
 define('HTTP_API', 'https://api.shopunity.net/v1/');
 
 define('DB_HOSTNAME', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', '');
+define('DB_PASSWORD', ''); // for mac user password 'root' 
 define('DB_PREFIX', 'oc_');
 define('DB_PORT', '3306');
 define('DB_NAME_START', 'opencart');
